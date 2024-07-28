0600565fb501bac619dfbab85221e82ade8470ce
Koushik Chetlur Rajesh <koushik112004@gmail.com>
2024-07-28 18:05:51 +0530

    Clerk, NeonDB, Drizzle, Auth, File Upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,41 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
+import { LogIn } from "lucide-react";
+import FileUpload from "@/components/FileUpload";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
   return (
-    <div className="w-screen min-h-screen"></div>
+    <div className="w-screen min-h-screen bg-gradient-to-l from-blue-300 to-pink-300">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">PDF Chat</h1>
+            <UserButton />
+          </div>
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join thousands of people to instantly get answers from PDF files by
+            asking questions to AI.
+          </p>
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <FileUpload />
+            ) : (
+              <Link href="/sign-in">
+                <Button>Login to get Started!
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
